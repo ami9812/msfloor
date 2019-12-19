c959913e7db63b783c713fa7c41ddd823ea48754
ami9812 <ami130103@gmail.com>
2019-12-19 13:27:10 +0900

    루트 common.php 호출
    페이지의 head.php와 tail.php 가 정상적으로 보여질 수 있도록 루트에 있는 common.php 파일을 호출

---
+++ page/certificate.php
@@ -0,0 +1,172 @@
+<?php
+include_once('./_common.php');
+
+$g5['title'] = '인증서';
+include_once('./_head.php');
+?>
+<link rel="stylesheet" href="<?php echo G5_URL; ?>/page/css/msfloor.css">
+
+<div id="navp">
+    <div class="navp_wr"><a href="<?php echo G5_URL ?>"><i class="fa fa-home"></i> HOME</a> &gt; 인증서 </div>
+</div>
+
+<!--<div id="nav">
+	<div class="nav_wr">
+		<a href="<?php echo G5_URL; ?>/page/introduce.php"><span class="other" style="border-left:1px solid #eee">CEO 인사말</span></a>
+		<a href="<?php echo G5_URL; ?>/page/ochart.php"><span class="other" style="border-right:1px solid #fff;">조직도</span></a>
+		<a href="<?php echo G5_URL; ?>/page/certificate.php"><span style="background: linear-gradient( to left, #2c9b41, #3b8ad6  );color:#fff">인증서</span></a>
+		<a href="<?php echo G5_URL; ?>/page/roadmap.php"><span class="other">오시는길</span></a>
+	</div>
+</div>-->
+
+<nav id="bo_cate">
+    <h2> 카테고리</h2>
+    <ul id="bo_cate_ul">
+        <li><a href="<?php echo G5_URL; ?>/page/introduce.php">CEO 인사말</a></li>
+        <li><a href="<?php echo G5_URL; ?>/page/ochart.php">조직도</a></li>
+        <li><a href="<?php echo G5_URL; ?>/page/certificate.php" id="bo_cate_on">인증서</a></li>
+        <li><a href="<?php echo G5_URL; ?>/page/roadmap.php">오시는길</a></li>
+    </ul>
+</nav>
+
+<!-- 타이틀 영역 --> 
+<div class="titleArea wow fadeInUp" data-wow-delay="0.1s">
+  <h2>인증서</h2>
+  <ul>
+  	<li>건축물 관련 여러 특허와 인증서입니다.</li>
+  </ul>
+</div>
+
+<div class="pagepan p11">
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s" style="margin-top:40px;border-bottom:2px solid #006600">
+	<h2 style="font-size:20px;margin-bottom:20px;"><i class="fa fa-chevron-circle-down" aria-hidden="true"></i> 안전관련 인증</h2>
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/safe/kc5.jpg"><div>MW-5시리즈 KC 인증</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/safe/kc7.jpg"><div>MW-7시리즈 KC 인증</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/safe/eu.agbb.jpg"><div>유럽 DIBt(AgBB) 인증</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/safe/eu.ce.jpg"><div>유럽 CE 인증</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/safe/us.floorscore.jpg"><div>미국 FloorScore인증</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/safe/ISO9001.jpg"><div>ISO 9001 인증</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/safe/ISO14001.jpg"><div>ISO 14001 인증</div></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s" style="margin-top:40px;border-bottom:2px solid #006600">
+	<h2 style="font-size:20px;margin-bottom:20px;"><i class="fa fa-chevron-circle-down" aria-hidden="true"></i> 시험성적서</h2>
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px;">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">유해성분 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/radon.jpg"><div>라돈 시험성적서</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/form.jpg"><div>폼알데하이드 시험성적서</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/resist.jpg"><div>방염성능 시험성적서</div></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/labela.jpg"><div>배출물등급A 시험성적서</div></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px;">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">CLASS AC3 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/ac3/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/ac3/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/ac3/0003.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/ac3/0004.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">CLASS AC4 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/ac4/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/ac4/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/ac4/0003.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/ac4/0004.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">치수 안정성 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/demension/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/demension/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/demension/0003.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">Fire BP1 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/fire/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/fire/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/fire/0003.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/fire/0004.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/fire/0005.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/fire/0006.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">Flame Spread 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/flame/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/flame/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/flame/0003.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">잔류 압입량 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/residual/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/residual/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/residual/0003.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">SGS VOC TVOC 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sgs/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sgs/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sgs/0003.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sgs/0004.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sgs/0005.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sgs/0006.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sgs/0007.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sgs/0008.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">SIA Phthalates 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sia/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sia/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/sia/0003.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">정적 하중 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/static/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/static/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/static/0003.jpg"></li>
+	</ul>	
+</div>
+<div class="centerc wow fadeInUp" data-wow-delay="0.4s">
+	<ul style="background: #f1f1f1;margin-top:20px">
+		<div style="font-size:1.3em;padding:10px 20px 10px 0;text-align: left; background: linear-gradient( to left, #3b8ad6, #003366  );width:250px;text-align: center;color:#fff">SVHC 시험성적서</div>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0001.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0002.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0003.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0004.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0005.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0006.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0007.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0008.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0009.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0010.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0011.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0012.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0013.jpg"></li>
+		<li><img src="http://msfloor.cdn3.cafe24.com/page/certificate/test/svhc/0014.jpg"></li>
+	</ul>	
+</div>
+</div>
+
+<?php
+include_once('./_tail.php');
+?>
